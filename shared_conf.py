1ca454b0f826313bf190268b11688a7a9de0f07f
xinetzone <xinzone@outlook.com>
2022-02-14 12:12:01 +0800

    modified:   _templates/footer.html

--- shared_conf.py
+++ shared_conf.py
@@ -47,7 +47,7 @@ extensions = [
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+    # "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
 intersphinx_disabled_domains = ["std"]
 
