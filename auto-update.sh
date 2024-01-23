f81c59bd39b1cc5036db2dae3b1b357ad817ddd5
Kay Hayen <kay.hayen@gmail.com>
2024-01-23 06:27:15 +0100

    Make the devcontainer launch serve site right away

---
+++ auto-update.sh
@@ -0,0 +1,8 @@
+python3 -m pip install -U invoke
+python3 -m invoke virtualenv
+# python3 -m invoke run -t update-docs
+python3 -m invoke doc
+# python3 -m invoke intl -l zh_CN
+# python3 -m invoke intl -l de_DE
+python3 -m invoke run -t post-process
+python3 -m invoke run -t serve-site
