0552d5fb177579dd406d864fcc54b94b55e09ee4
Kay Hayen <kay.hayen@gmail.com>
2024-03-12 15:30:04 +0100

    Use system font for Roboto slab too

--- update.py
+++ update.py
@@ -785,6 +785,13 @@ def runPostProcessing():
                 )
                 merged_css = merged_css.replace("Lato", "ui-sans-serif")
                 merged_css = re.sub(
+                    r"@font-face\{([^)]*?Roboto Slab)(.*?)\}",
+                    r"",
+                    merged_css,
+                    flags=re.S,
+                )
+                merged_css = merged_css.replace("Roboto Slab", "Rockwell, 'Rockwell Nova','Roboto Slab','DejaVu Serif','Sitka Small',serif")
+                merged_css = re.sub(
                     r"@font-face\{(.*?)\}",
                     r"@font-face{font-display:swap;\1}",
                     merged_css,
