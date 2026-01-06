12ccb085981083cec69b101285c7e121a97f0fc8
Daury Pérez <dauryjey@gmail.com>
2026-01-06 21:42:26 +0000

    Bundle basic conf

--- bundle/conf.py
+++ bundle/conf.py
@@ -1,56 +1,29 @@
-# Obtain shared config values
 import sys
 from pathlib import Path
 
 DOC_ROOT = Path(__file__).parent.absolute()
 sys.path.extend([DOC_ROOT.as_posix(), DOC_ROOT.parent.as_posix()])
 
-# isort:start
-
-# -- General configuration
 from shared_conf import *
 
-# For autodoc to work we need to import Nuitka
-from nuitka_import import importNuitka  # isort:skip
+# Offline bundle setup
+html_baseurl = "./"
+master_doc = "index_bundle"
 
-nuitka = importNuitka()
-
-extensions += [
-    # Blog extension
-    "ablog",
+# Keep only the bundle content, everything else is excluded
+include_patterns = [
+	"index_bundle.rst",
+	"variables.inc",
+	"dynamic.inc",
+	"doc/**",
+	"user-documentation/**",
 ]
 
-# Options for ABlog
-
-# The "title" for the blog, used in active pages.  Default is ``'Blog'``.
-blog_title = "Nuitka Blog"
-
-# Base URL for the website, required for generating feeds.
-# e.g. blog_baseurl = "http://example.com/"
-blog_baseurl = html_baseurl
-blog_path = "blog"
-blog_feed_fulltext = True
-blog_feed_archives = True
-blog_feed_length = None
-
-# TODO: Optimized feeds one day?
-x_blog_feed_templates = {
-    # Use defaults, no templates
-    "atom": {},
-    # Create content text suitable posting to social media
-    "social": {
-        # Format tags as hashtags and append to the content
-        "content": "{{ title }}{% for tag in post.tags %}"
-        " #{{ tag.name|trim()|replace(' ', '') }}"
-        "{% endfor %}",
-    },
+# Remove online only or site only extensions
+excluded_extensions = {
+	"ablog",
+	"sphinx_sitemap",
+	"carousel",
 }
 
-# Sitemap configuration
-sitemap_locales = [None]
-sitemap_url_scheme = "{link}"
-
-autodoc_member_order = "bysource"
-
-# Sphinx intl configuration
-gettext_compact = False
+extensions = [ext for ext in extensions if ext not in excluded_extensions]
